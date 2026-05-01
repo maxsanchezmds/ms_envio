337c7677185b1ac68e45b1943fd718438953a05b
Bastian <bastian@smartlogix.com>
2026-05-01 17:03:16 -0400

    [feat] crear entidad Envio con TypeORM

---
+++ src/envios/envio.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum EstadoEnvio {
+  PENDIENTE = 'pendiente',
+  EN_TRANSITO = 'en_transito',
+  ENTREGADO = 'entregado',
+  CANCELADO = 'cancelado',
+}
+
+@Entity('envios')
+export class Envio {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  pedidoId: string;
+
+  @Column()
+  direccionDestino: string;
+
+  @Column()
+  ciudadDestino: string;
+
+  @Column({ type: 'enum', enum: EstadoEnvio, default: EstadoEnvio.PENDIENTE })
+  estado: EstadoEnvio;
+
+  @Column({ nullable: true })
+  transportista: string;
+
+  @Column({ nullable: true })
+  codigoSeguimiento: string;
+
+  @CreateDateColumn()
+  creadoEn: Date;
+
+  @UpdateDateColumn()
+  actualizadoEn: Date;
+}
